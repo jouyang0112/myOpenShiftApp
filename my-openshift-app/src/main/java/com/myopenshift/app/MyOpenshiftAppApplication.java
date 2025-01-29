d4d65a105380b20af96250a20b950f4d322b1292
unihorn jun <jun@unihorn.com>
2025-01-28 23:40:56 -0500

    first commit

---
+++ my-openshift-app/src/main/java/com/myopenshift/app/MyOpenshiftAppApplication.java
@@ -0,0 +1,13 @@
+package com.myopenshift.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOpenshiftAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOpenshiftAppApplication.class, args);
+	}
+
+}
---
+++ my-openshift-app/src/test/java/com/myopenshift/app/MyOpenshiftAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myopenshift.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOpenshiftAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
